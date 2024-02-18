a2cee67c58b3c2a865953517f0200ad49f6c34e8
adnan4k <fayomuhe5@gmail.com>
2024-02-18 10:08:29 +0300

    setup complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+ Route::get('/register/seeker',[UserController::class,'createSeeker']);
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/users', function () {
+    return view('user.index');
+});
+
