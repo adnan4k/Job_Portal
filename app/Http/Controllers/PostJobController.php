5b3514080c4160fae54a5fd6c5347cebdbb646a1
adnan4k <fayomuhe5@gmail.com>
2024-03-06 06:58:53 +0300

    update post completed

--- app/Http/Controllers/PostJobController.php
+++ app/Http/Controllers/PostJobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EditFormRequest;
 use App\Models\Listing;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
@@ -55,5 +56,21 @@ class PostJobController extends Controller
         $posting->save();
 
         return back();
+    }
+
+    public function edit(Listing $listing){
+
+        return view('job.edit',compact('listing'));
+    }
+
+    public function update($id,EditFormRequest $request){
+        if($request->hasFile('feature_image')){
+            $imagePath = $request->file('feature_image')->store('images', 'public');
+            Listing::find($id)->update(['feature_image',$imagePath]);
+        }
+
+        Listing::find($id)->update($request->except('feature_image'));
+
+        return back()->with('success','Your job is successfully update');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,8 @@ use Stripe\Subscription;
  Route::get('/payment/cancel',[SubscriptionController::class,'cancel'])->name('payment.cancel');
  Route::get('/job/create',[PostJobController::class,'create'])->name('job.create')->middleware(isPremiumUser::class);
  Route::post('/job/store',[PostJobController::class,'store'])->name('job.store')->middleware(isPremiumUser::class);
+ Route::get('/job/{listing}/edit',[PostJobController::class,'edit'])->name('job.edit')->middleware(isPremiumUser::class);
+ Route::put('/job/{id}/update',[PostJobController::class,'update'])->name('job.update')->middleware(isPremiumUser::class);
 
 
 
